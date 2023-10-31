99abfd2c5eb03c0133da38b0fc72356280fe6111
alexgm5555 <alexgm5555@hotmail.com>
2023-10-31 16:23:54 -0500

    feat: fix main page style

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,20 +1,24 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
 
 import './styles.scss';
 
 import { AllProducts, SelectedProducts, ShoppingCar } from '../../components';
 
 export const Main:FC = () => {
+  const [ ...data ] = useSelector((state: any) => state.user.products);
   return (
     <div className='Main-container'>
       <div className='children-container'>
         <div className='panel-left'>
           <AllProducts/>
         </div>
-        <div className='panel-right'>
-          <ShoppingCar/>
-          <SelectedProducts/>
-        </div>
+        {data.length  !== 0 && 
+          <div className='panel-right'>
+            <ShoppingCar/>
+            <SelectedProducts/>
+          </div>
+        }
       </div>
     </div>
   );
